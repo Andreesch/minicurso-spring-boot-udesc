1f719debbb9dbcef820bb6e4ea7bb11aeea4cbe5
ronaldo_fll <ronaldo@alpdex.com.br>
2020-02-11 20:01:45 -0300

    Criado controller

--- src/main/java/com/example/myorder/services/RestaurantService.java
+++ src/main/java/com/example/myorder/services/RestaurantService.java
@@ -1,8 +1,25 @@
 package com.example.myorder.services;
 
+import com.example.myorder.api.dtos.CreateRestaurantDto;
+import com.example.myorder.entities.Restaurant;
+import com.example.myorder.repositories.RestaurantRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RestaurantService {
 
+    @Autowired
+    private RestaurantRepository restaurantRepository;
+
+    public void createRestaurant(CreateRestaurantDto createRestaurantDto) {
+        //TODO validações
+
+        Restaurant restaurant = new Restaurant()
+                .setEmail(createRestaurantDto.getEmail())
+                .setName(createRestaurantDto.getName())
+                .setPhone(createRestaurantDto.getPhone());
+
+        restaurantRepository.save(restaurant);
+    }
 }
