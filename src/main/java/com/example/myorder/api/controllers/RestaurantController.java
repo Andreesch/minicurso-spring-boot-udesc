946d9697d1b72444f2e59c4e5839cb103d2a0834
ronaldo_fll <ronaldo@alpdex.com.br>
2020-02-11 20:40:37 -0300

    Criado getRestaurant

--- src/main/java/com/example/myorder/api/controllers/RestaurantController.java
+++ src/main/java/com/example/myorder/api/controllers/RestaurantController.java
@@ -2,9 +2,11 @@ package com.example.myorder.api.controllers;
 
 import com.example.myorder.api.RestPath;
 import com.example.myorder.api.dtos.CreateRestaurantDto;
+import com.example.myorder.api.dtos.RestaurantResponseDto;
 import com.example.myorder.services.RestaurantService;
 import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
@@ -25,5 +27,9 @@ public class RestaurantController {
         restaurantService.createRestaurant(createRestaurantDto);
     }
 
-
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping()
+    public RestaurantResponseDto get(@RequestParam @Param("id") Integer id) {
+        return restaurantService.getById(id);
+    }
 }
--- src/main/java/com/example/myorder/services/RestaurantService.java
+++ src/main/java/com/example/myorder/services/RestaurantService.java
@@ -1,10 +1,13 @@
 package com.example.myorder.services;
 
 import com.example.myorder.api.dtos.CreateRestaurantDto;
+import com.example.myorder.api.dtos.RestaurantResponseDto;
 import com.example.myorder.entities.Restaurant;
 import com.example.myorder.repositories.RestaurantRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class RestaurantService {
@@ -21,5 +24,16 @@ public class RestaurantService {
                 .setPhone(createRestaurantDto.getPhone());
 
         restaurantRepository.save(restaurant);
+    }
+
+    public RestaurantResponseDto getById(Integer id) {
+        Optional<Restaurant> optional = restaurantRepository.findById(id);
+        Restaurant restaurant =  optional.get();
+        return new RestaurantResponseDto()
+                .setId(restaurant.getId())
+                .setEmail(restaurant.getEmail())
+                .setName(restaurant.getName())
+                .setPhone(restaurant.getPhone());
+
     }
 }
