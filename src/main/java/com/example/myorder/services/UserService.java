f0f6d9c7b58e6eda3a0044f611dcfa3c32c7e66f
ronaldo_fll <ronaldo@alpdex.com.br>
2020-02-12 20:02:53 -0300

    Criação de usuário

--- src/main/java/com/example/myorder/services/UserService.java
+++ src/main/java/com/example/myorder/services/UserService.java
@@ -4,6 +4,7 @@ import com.example.myorder.api.dtos.CreateUserDto;
 import com.example.myorder.api.dtos.UserResponseDto;
 import com.example.myorder.api.mappers.UserMapper;
 import com.example.myorder.entities.User;
+import com.example.myorder.exceptions.AlreadyExistsException;
 import com.example.myorder.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,6 +16,27 @@ public class UserService {
     private UserRepository userRepository;
 
     public UserResponseDto create(CreateUserDto createUserDto) {
+        validateUserEmail(createUserDto.getEmail());
 
+        User user = userRepository.save(createUser(createUserDto));
+
+       return UserMapper.toResponseDto(user);
+    }
+
+    private User createUser(CreateUserDto createUserDto) {
+        return new User()
+                .setName(createUserDto.getName())
+                .setEmail(createUserDto.getEmail())
+                .setAddress(createUserDto.getAddress())
+                .setPassword(createUserDto.getPassword())
+                .setPhone(createUserDto.getPhone());
+    }
+
+    private void validateUserEmail(String email) {
+        User user = userRepository.findByEmail(email);
+
+        if (user != null) {
+            throw new AlreadyExistsException("já existe um usuário cadastrado com este email");
+        }
     }
 }
