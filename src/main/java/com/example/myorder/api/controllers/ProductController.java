bd61560fe0af60e14025941ed13816f5f8616734
ronaldo_fll <ronaldo@alpdex.com.br>
2020-02-13 20:13:04 -0300

    Criando controller produto

---
+++ src/main/java/com/example/myorder/api/controllers/ProductController.java
@@ -0,0 +1,29 @@
+package com.example.myorder.api.controllers;
+
+import com.example.myorder.api.RestPath;
+import com.example.myorder.api.dtos.ProductResponseDto;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(RestPath.BASE_PATH + "/product")
+public class ProductController {
+    
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping()
+    @ApiResponses({
+            @ApiResponse(code = 201, message = "Produto criado!", response = ProductResponseDto.class)
+    })
+    public ProductResponseDto create() {
+        return null; //TODO chamar productService
+    }
+
+
+}
